8b08e2bfffa7453928864bf31bc4087616d06671
Sara Z Cheng <sarazqcheng@gmail.com>
2015-01-30 10:48:41 -0800

    add review bug

--- app/recipe-detail/recipe-detail.js
+++ app/recipe-detail/recipe-detail.js
@@ -16,11 +16,15 @@ angular.module('myApp.recipeDetail', ['ngRoute'])
 
         $scope.recipeId = $routeParams.recipeId;
 
+
+        //get one particular recipe
         Restangular.one('recipes', $scope.recipeId).customGET().then(function (data) {
             $scope.recipe = data;
 
         });
 
+
+        //delete a recipe
         $scope.deleteRecipe = function() {
         var confirmation = confirm('Are you sure you want to delete this recipe? This cannot be undone');
 
@@ -33,7 +37,37 @@ angular.module('myApp.recipeDetail', ['ngRoute'])
                     alert('There was a problem deleting your recipe')
                 })
             }
-        }
+        };
 
-    }]);
+
+        //Add new review under one particular recipe
+        var newReview={
+            recipe: $routeParams.recipeId,
+            title: $scope.reviewTitle,
+            username: $scope.reviewUserName,
+            reviews: $scope.reviewContent
+
+        };
+
+        $scope.successMessage = "";
+
+        $scope.addReview = function() {
+
+            Restangular.one('add-review/').customPOST(newReview).then(function() {
+                console.log(newReview);
+                $scope.successMessage = "Success";
+                $scope.recipe.reviews.unshift(reviews);
+
+          });
+        };
+
+
+        //show the addReview form or hide it
+        $scope.reviewform = true;
+
+        $scope.toggle = function(){
+            $scope.reviewform = !$scope.reviewform;
+            return $scope.reviewform
+        };
+  }]);
 
